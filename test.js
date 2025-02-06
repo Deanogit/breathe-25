b9654912d354535bb03ced1fd0501c108856a526
Dean Howe <hideanhowe@gmail.com>
2025-02-06 19:11:01 +0000

    test continues

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const content = document.getElementById('content');
 const beginBtn = document.getElementById('begin-btn');
+const duration = document.getElementById('duration');
 
 function animation() {
   content.style.opacity = 0;
@@ -7,7 +8,7 @@ function animation() {
     content.innerHTML = `
         <h2>Choose how long you'd like to practice for</h2>
         <nav>
-        <ul>
+        <ul id="duration">
         <li><a id="time1" href="#1">1 min</a></li>
         <li><a id="time2" href="#2">2 min</a></li>
         <li><a id="time3" href="#3">3 min</a></li>
@@ -20,6 +21,16 @@ function animation() {
   setTimeout(() => {
     content.style.opacity = 1;
   }, 3000);
+  duration.addEventListener('click', time(e));
+}
+
+function time(e) {
+  console.log(e.target.id);
 }
 
 beginBtn.addEventListener('click', animation);
+
+// getting an error here...
+// test.js:24 Uncaught TypeError: Cannot read properties of
+// null (reading 'addEventListener')
+// at HTMLButtonElement.animation (test.js:24:12)
